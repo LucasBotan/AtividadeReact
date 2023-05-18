66dad8e066e14325dc4044998fb269214ae79ff0
LucasBotan <lucasmaiden@gmail.com>
2023-05-17 23:42:10 -0300

    atualizando o app.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomePage from './src/pages/Home';
+import LoginPage from './src/pages/Login';
+import EditUserPage from './src/pages/EditUser';
+
+import EditRolePage from './src/pages/EditRole';
+import ListRolePage from './src/pages/ListRole';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>      
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginPage} options={{title: 'Acesso' }} />
+        <Stack.Screen name="Home" component={HomePage} options={{title: 'Home Page' }}/>      
+        <Stack.Screen name="EditUser" component={EditUserPage} options={{title: 'Edit User' }} />   
+        <Stack.Screen name="EditRole" component={EditRolePage} options={{title: 'Edit Role' }}/>     
+        <Stack.Screen name="ListRole" component={ListRolePage} options={{title: 'Role List' }}  />       
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
